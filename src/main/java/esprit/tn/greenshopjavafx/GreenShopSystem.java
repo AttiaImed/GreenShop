c0c50aae1c13db9eb36d7189b568c653223f3abc
attiaimed <imed.attia@esprit.tn>
2024-01-19 10:10:30 +0100

    Project Closed

--- src/main/java/esprit/tn/greenshopjavafx/GreenShopSystem.java
+++ src/main/java/esprit/tn/greenshopjavafx/GreenShopSystem.java
@@ -6,7 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class imedGreenShopSystem extends Application {
+public class GreenShopSystem extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
